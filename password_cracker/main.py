d5c543346ce7186a715e59488838c941080eabb0
Bilal Ayakdaş <bilalayakdas@gmail.com>
2024-12-24 23:46:14 +0300

    Added multiprocess

--- password_cracker/main.py
+++ password_cracker/main.py
@@ -22,7 +22,7 @@ def check_password_request(password):
 
 def generate_password():
     password = "".join(
-        random.choices(string.ascii_letters + string.digits, k=random.randint(8, 16))
+        random.choices(string.ascii_letters + string.digits, k=random.randint(2, 4))
     )
     hashed_password = hashlib.md5(password.encode()).hexdigest()
     return  password,hashed_password
@@ -36,12 +36,15 @@ def crack_password():
         if hashed_password == md5_hash:
             with lock:
                 print(f"Password found: {password}")
+                response = requests.post(f"{BASE_URL}/check_password", json={"password": password})
+                print(response.json())
+
             break
 
 if __name__ == '__main__':
     get_password_from_api()
 
-    thread_count = 5
+    thread_count = 6
     threads = []
 
     for _ in range(thread_count):
